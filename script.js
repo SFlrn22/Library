8f0066d874785dad867ccf1386cc06ae6a36eebc
Florin <109854105+SFlrn22@users.noreply.github.com>
2023-02-24 12:35:40 +0200

    Add function to remove all books

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const formWrap = document.getElementById('form-wrap');
 const addBtn = document.getElementById('addb');
 const cancelBtn = document.getElementById('cancel');
+const removeAllBtn = document.getElementById('remove-all');
 const library = document.querySelector('.library');
 const navbar = document.querySelector('.navbar');
+const formTitle = document.getElementById('title');
+const formAuthor = document.getElementById('author');
+const formPages = document.getElementById('pages');
+const formCheckbox = document.getElementById('checkbox');
+const shownBooks = document.querySelectorAll('.book-card');
+
+let libraryArr = [];
 
 function openForm() {
   formWrap.style.display = 'flex';
@@ -16,5 +24,18 @@ function closeForm() {
   navbar.style.opacity = '1';
 }
 
+function resetForm() {
+  formTitle.textContent = '';
+  formAuthor.textContent = '';
+  formPages.textContent = '';
+  formCheckbox.checked = false;
+}
+
+function removeAll() {
+  shownBooks.forEach((elem) => elem.remove());
+  libraryArr = [];
+}
+
 addBtn.addEventListener('click', openForm);
 cancelBtn.addEventListener('click', closeForm);
+removeAllBtn.addEventListener('click', removeAll);
